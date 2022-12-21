954bffbdb1d707684aa953270219e53d9d4d5133
arXaik <araik200414@gmail.com>
2022-12-21 19:30:05 +0200

    Добавьте файлы проекта.

---
+++ 4.1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Петросян 4,1 средний уровень 22-Исп-2/1 
+int[] a = new int[10];
+int sum = 0, count = 0;
+Console.WriteLine("Введит числа в массив:");
+for (int i = 0; i < 10; i++)
+{
+    a[i] = int.Parse(Console.ReadLine());
+}
+foreach (int i in a)
+{
+    if (i > 0)
+    {
+        count++;
+        sum = sum + a[i];
+    }
+    else break;
+}
+
+Console.WriteLine($"Cумма элементов до отрицательного:{sum}");
+Console.WriteLine($"Кол-во элементов до отрицательного:{count}");
